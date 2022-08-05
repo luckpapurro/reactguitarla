884643333c14ececc2116b31b5def397bd1ed4c4
Lucas Rombini <83418676+luckpapurro@users.noreply.github.com>
2022-08-04 23:57:02 -0300

    22/08/04 HC 201

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -32,8 +32,7 @@ const Nosotros = () => {
               tortor sed gravida facilisis. Fusce malesuada et nisi et dictum.
               Mauris eget mi a enim tincidunt pulvinar. Aenean sagittis velit
               eleifend tincidunt lobortis. Cras imperdiet metus eget nisi
-              tincidunt, quis imperdiet dolor iaculis. Proin ac massa sed lectus
-              mollis viverra malesuada non ex.
+              tincidunt, quis imperdiet dolor iaculis.
             </p>
           </div>
         </div>
